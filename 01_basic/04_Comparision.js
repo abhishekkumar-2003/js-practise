a6153184aa0972bf3fc90d56a51d24937b80eeb9
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-18 23:45:39 +0000

    comparision operator

---
+++ 01_basic/04_Comparision.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+console.log("2" > 1);
+console.log("2" < 1);
+console.log("bulbul" > "abhishek");
+console.log("zzzw">"zzzt");
+//"z">"x"
+console.log("---------------")
+console.log(null==0);
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0);
+console.log("---------------")
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+
+
