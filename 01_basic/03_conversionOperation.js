a07fbf322b51d6f1e487c8983af575417d90b112
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-18 10:40:18 +0000

    Conversion Operation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score = 33;
+console.log(typeof score);
+console.log(score);
+
+console.log("--------------------");
+
+let score1= "33";
+console.log(typeof (score1));
+console.log(typeof(Number(score1)));
+console.log(score1);
+console.log(Number(score1));
+
+console.log("--------------------");
+
+
+let score2= "33abc";
+console.log(typeof score2);
+console.log(typeof (Number(score2)));
+console.log(score2);
+console.log(Number(score2));
+
+console.log("--------------------");
+
+let score3= null;
+console.log(typeof score3);
+console.log(typeof (Number(score3)));
+console.log(score3);
+console.log(Number(score3));
+
+console.log("--------------------");
+
+let score4= undefined;
+console.log(typeof score4);
+console.log(typeof (Number(score4)));
+console.log(score4);
+console.log(Number(score4));
+
+console.log("--------------------");
+
+let score5= true;
+console.log(typeof score5);
+console.log(typeof (Number(score5)));
+console.log(score5);
+console.log(Number(score5));
