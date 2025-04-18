f30a0c8a13ac54fce33559c5e35fe1d657f9f38a
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-18 05:31:37 +0000

    dataType

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+console.log("Abhi");
+const customerId = 5;
+let varID = "xtz";
+
+var accountPass = "A11s02" ;
+accountID = 117;
+//customerId = 79;
+console.log(customerId);
+/*
+Prefer not to use var b/c of issue in block scope & functional scope
+*/
+console.log(accountID);
+let acountstate;
+console.log(acountstate);
+
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,17 @@
+"use strict";//treate as a newer version of java script
+
+// alert (3+3); // we are useing node js not browser so we cannot directly run this code
+
+let name = "Abhishek";//string
+let age = 21;//int
+// bigint => larger value
+let isCoder = true; // boolean
+let getPlaced;//undifine
+let exceptationSalary = null //null => standalone value
+//symol => unique
+
+
+console.log(typeof "abhi");
+console.log(typeof age)
+console.log(typeof null)//type: object
+console.log(typeof undefined)//type: undefine
