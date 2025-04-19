d4f6dc88382f84c2aa02f369f91f179ea4ecf25b
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-19 03:27:23 +0000

    typeof

--- 01_basic/dataType-Summary.js
+++ 01_basic/dataType-Summary.js
@@ -10,30 +10,42 @@ non-primitive
 
 const score = 101;
 const scorePer = 75.9;
+console.log(typeof score);//Number
+console.log(typeof scorePer);//Number
 
 const isLogged = false;
+console.log(typeof isLogged);//boolean
+
 const outSideTemp = null;
+console.log(typeof outSideTemp);//object
 
 let userMail;
 
 const id = Symbol('123');
 const idPass = Symbol('123');
+console.log(typeof id);//symbol
 console.log(id===idPass);//false
 
 const bigNumber = 23456789876543n;//BigInt
+console.log(typeof bigNumber);//bigint
 
 /* Non-Primitive -> Reference Type 
 -> Arrays; object;  Functions;
 */
 const heroes = ["saktiman", "nagagraj", "doga"];
+console.log(typeof heroes);//object
+
 let myObj = {
     name: "abhi",
     age : 18,
 }
+console.log(typeof myObj);//object
 const myFun = function(){
     console.log("Hello java script");
     
 }
+console.log(typeof myFun);//function
+
 /*
 JavaScript is a dynamically typed language.
 
