1ea8bd80b7d652f85df99071bd0e805d117df330
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-19 06:00:46 +0000

    basic string

---
+++ 01_basic/05_Sting.js
@@ -0,0 +1,12 @@
+const player = "Noob Pro";
+console.log(player);
+console.log(player.length);
+console.log(player.charAt(7));
+console.log(player.indexOf('o'));
+
+const gameName = new String(`smash cars`);
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName.charAt(7));
+console.log(gameName.indexOf('a'));
+console.log(gameName.__proto__);
