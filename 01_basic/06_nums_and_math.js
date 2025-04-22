a7537b63dd53eb3b06393a3f4424c59294d1e618
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-22 08:45:38 +0000

    num

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,26 @@
+const score = 100;
+//console.log(score);
+
+let scoree = new Number(1001);
+// console.log(scoree);
+// console.log(scoree.toString().length);
+
+const pie = 22/7;
+// console.log(pie.toFixed(2));
+// console.log(pie.toPrecision(5));
+
+const largeBill = 194397302847;
+// console.log(largeBill.toLocaleString(`en-IN`));
+
+//++++++++++++ MATHS +++++++++++++++++++++++++
+
+
+
+// console.log(Math);
+// console.log(Math.max(2, 3, 6, 7, 5, 9, 9.1));
+// console.log(Math.min(2, 3, -2, 8, 9, -2.1));
+// console.log(Math.floor(4.8));
+// console.log(Math.ceil(4.2));
+// console.log(Math.round(4.5));
+// console.log(Math.abs(-99.65));
+
