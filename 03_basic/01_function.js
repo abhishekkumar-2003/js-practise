e7d92309c4556ec2736dcc346d0387f127d2bd03
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-23 19:26:25 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,13 @@
+// function sayMyName() {//here funtion is a keyword & sayMyName is a function name
+//     console.log("A");
+//     console.log("B");
+//     console.log("H");
+//     console.log("I");
+// }
+//sayMyName()
+function addTwoNumber(num1, num2){//here num1 and num2 is parameter
+    console.log(num1+num2);
+}
+addTwoNumber(1,5);//here we are calling function and here 1 and 5 is argument
+const res = addTwoNumber(1,2);
+console.log("res: "+res);//we do not return any things thats why it return undefine
