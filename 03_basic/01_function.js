62d8d5c37b0ba66ee2ae651695029c9767130370
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-23 21:29:24 +0000

    funtion with object and arrays

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -5,9 +5,54 @@
 //     console.log("I");
 // }
 //sayMyName()
-function addTwoNumber(num1, num2){//here num1 and num2 is parameter
-    console.log(num1+num2);
+
+
+// function addTwoNumber(num1, num2){//here num1 and num2 is parameter
+//     console.log(num1+num2);
+// }
+// addTwoNumber(1,5);//here we are calling function and here 1 and 5 is argument
+// const res = addTwoNumber(1,2);
+// console.log("res: "+res);//we do not return any things thats why it return undefine
+// function multiplyTwoNum(num3, num4) {
+//     const resMul = num3*num4;
+//     return resMul;
+// }
+// console.log(multiplyTwoNum(6,9));//now this will return value
+
+
+
+function logInUser(username){
+    if(username===undefined){
+        //console.log("Please enter the user name");
+        return `please enter the user name`;
+    }
+    return`${username} just loged in`;
 }
-addTwoNumber(1,5);//here we are calling function and here 1 and 5 is argument
-const res = addTwoNumber(1,2);
-console.log("res: "+res);//we do not return any things thats why it return undefine
+//console.log(logInUser());
+//console.log(logInUser("abhi"));
+
+
+//+++++++++Rest Operator+++++++++++
+function calculatePrice(...value1) {//...value1 here ... work as a rest operater which can take multiple parameter without initial difination
+    return value1;
+}
+//console.log(calculatePrice(122, 334, 45,"abhi", 678, 9));
+
+const visitor = {
+    userName_vi : "Surbhi",
+    profession : "Developer"
+};
+function handleObject(anyObject) {
+    console.log(`${anyObject.userName_vi} is a ${anyObject.profession}`);
+};
+handleObject(visitor);
+handleObject({
+    userName_vi:"Abhishek",
+    profession: "Coder"
+});
+
+const myArray = [100, 200, 300, 400, 500];
+function returnSecondValue(any_Array) {
+    return any_Array[1];
+}
+console.log(returnSecondValue(myArray));
