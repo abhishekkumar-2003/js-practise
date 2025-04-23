970d4ae6de2337447206b0f7af46c286a8b76058
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-23 08:33:26 +0000

    basic part of array

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,38 @@
+// const newArray = [1, 2, 3, 4, 5, 6];//0 Based indexing
+// const newArray2=["ironman", "woderwomen", "saktiman"];
+// const newArray3 = newArray+newArray2;
+// console.log(newArray3);//arrays can have string, number and boolean value toghether  
+// const myarray = new Array(1, 2, 6, 8, 9);
+// console.log(myarray[0],myarray[2]);
+
+//Array Method
+const newMyArray = [9, 8,7,6,5,4,3,1];
+// console.log(newMyArray.includes(8));//return the values true or false
+// console.log(newMyArray.indexOf(9));//return index if present otherwise return -1
+
+
+// console.log(newMyArray);
+// newMyArray.push(115);
+// console.log(newMyArray);
+// newMyArray.push(0, 91);
+// console.log(newMyArray);
+// newMyArray.pop();
+// console.log(newMyArray);
+// newMyArray.unshift(0);
+// console.log(newMyArray);
+// newMyArray.unshift(111);//add number in 0th potision
+// console.log(newMyArray);
+// newMyArray.shift();// remove digit from 0th position
+// console.log(newMyArray);
+
+// const array_join = newMyArray.join();//convert object into string
+// console.log(newMyArray);
+// console.log(array_join);
+// console.log(typeof newMyArray);//output: object
+// console.log(typeof array_join);//output: string
+
+console.log("A =" + newMyArray);
+console.log(newMyArray.slice(1, 3));
+console.log("B "+newMyArray);
+console.log(newMyArray.splice(1,3));
+console.log("C "+newMyArray);
