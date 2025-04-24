d77a039230ffc1222959b8cdad97b08af37a2e1f
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-24 05:39:33 +0000

    global and block scope

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -16,4 +16,25 @@ console.log("(globle scope)b: "+b);
 console.log("(globle scope)c: "+c);
 //value of globle scope var c change due to block scope which can lead to error
 
+function one() {
+    const usserNam = "Abhishek Kumar";
+    function two() {
+        const website = "Youtube";
+        console.log(usserNam);
+        
+    }
+    //console.log(website);//we can not acces the variable of nested fuction outside the function
+    two();
+}
+one();
 
+//----------------intresting--------------
+console.log(addONE(5));//we can call the funtion before initialization if we make a function directly
+function addONE(num) {
+    return num+1;
+}
+//console.log(addTWO(8));//if we store funtion inside variable we can not call the funtion before initialization
+
+const addTWO = function (num) {
+    return num +2;
+}
