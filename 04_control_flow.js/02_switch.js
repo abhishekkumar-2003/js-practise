68d331c5488b339e01c9006db09716843505d9a5
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-28 21:14:20 +0000

    24-04-2025

---
+++ 04_control_flow.js/02_switch.js
@@ -0,0 +1,43 @@
+const month =7;
+switch (month) {
+    case 1:
+        console.log("jaunwary");
+        break;
+    case 2:
+        console.log("feburary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("may");
+        break;
+    case 5:
+        console.log("april");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("october");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+        
+
+    default:
+        break;
+}
