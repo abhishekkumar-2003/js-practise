1be55516addbad175ccc8d47e48bddc337b3b83d
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-29 11:30:57 +0000

    do while

---
+++ 05_iteration/02_while_loop.js
@@ -0,0 +1,23 @@
+//while loop
+
+// let i = 0;
+// while (i<=10) {
+//     console.log(`the value oa i after increment of 2 ia ${i}`);
+//     i=i+2;
+// }
+// i=0;
+
+// let myArray = ["mango", "apple", "banana", "papaya", "orange"];
+
+// while (i<myArray.length) {
+//     console.log(`value at ${i} is ${myArray[i]}`);
+//     i++;
+// }
+
+//do while loop
+let score = 17;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=12);
+//at least once loop will run if value will print or not
