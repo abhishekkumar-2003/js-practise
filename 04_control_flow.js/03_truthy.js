c9ea9bf4bc3752f0676a8241ae80b37fc57e4d78
Abhishek Kumar <124602751+abhishekkumar-2003@users.noreply.github.com>
2025-04-29 06:50:55 +0000

    ternary operartor

--- 04_control_flow.js/03_truthy.js
+++ 04_control_flow.js/03_truthy.js
@@ -41,3 +41,44 @@ Functions — even if empty — are also truthy.
 =>true	          Boolean	  true
 
 */
+
+const emptyObj = {};
+if(Object.keys(emptyObj).length === 0){
+    console.log("Empty object ");
+    //console.log(Object.keys(emptyObj));
+     
+}
+
+//Nullish Coalescing Operator (??)
+/*
+Purpose:
+It provides a default value when the original value is null or undefined.
+
+Syntax:
+
+javascript
+Copy
+Edit
+let result = value ?? defaultValue;
+It checks:
+
+If value is NOT null AND NOT undefined → returns value.
+
+Otherwise → returns defaultValue.
+*/
+console.log(null ?? "Hello");     // "Hello"
+console.log(undefined ?? "World"); // "World"
+console.log(false ?? "Hi");        // false (NOT "Hi")
+console.log(0 ?? 100);             // 0 (NOT 100)
+console.log('' ?? "Empty");        // '' (empty string)
+
+
+//terniary operator
+/*
+condition ? expressionIfTrue : expressionIfFalse;
+condition: An expression that evaluates to true or false.
+
+expressionIfTrue: Executed if the condition is true.
+
+expressionIfFalse: Executed if the condition is false.
+ */
